765c664f2b40c45d5045353b7278a52670bbb842
Fajar <kucaykojek@gmail.com>
2023-08-27 17:38:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,21 @@
+# Kucaykojek's Website
+
+This is just a website of **Asep Fajar Nugraha**.
+
+- Email: [delve_brain@hotmail.com](mailto:delve_brain@hotmail.com)
+- Linkedin: [https://www.linkedin.com/in/kucaykojek](https://www.linkedin.com/in/kucaykojek)
+
+### Setup
+
+Set env and install depedencies.
+```
+cp env.sample .env && npm i
+```
+
+### Packages
+
+- CSS framework: [tailwindcss](https://tailwindcss.com)
+- Icon library: [lucide](https://lucide.dev)
+- Code Quality:
+  - [husky](https://typicode.github.io/husky)
+  - [prettier](https://tailwindcss.com)
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        'primary-shade': 'var(--primary-shade)'
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+        mono: ['var(--font-mono)', ...fontFamily.mono]
+      },
+      animation: {
+        'logo-spin': 'spin 20s linear infinite'
+      }
+    }
+  },
+  plugins: []
+}
+export default config
