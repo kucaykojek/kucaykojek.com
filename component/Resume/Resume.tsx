b45f1833da02641b16d078665a09ab0e09e34331
Fajar <kucaykojek@gmail.com>
2023-09-21 10:47:40 +0700

    chore: add resume stacks

--- component/Resume/Resume.tsx
+++ component/Resume/Resume.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Image from 'next/image'
-import { useMemo } from 'react'
 
 import Loader, { useLoader } from '@/component/Loader'
 import Logo from '@/component/Logo'
@@ -18,7 +17,7 @@ type TPeriod = {
 export default function Resume() {
   const { initial } = useLoader()
 
-  const periods: TPeriod[] = [
+  const experiences: TPeriod[] = [
     {
       years: [2010],
       summary:
@@ -54,10 +53,8 @@ export default function Resume() {
     }
   ]
 
-  const lastYear: number = useMemo(() => {
-    const lastYears = periods[periods.length - 1].years
-    return lastYears[lastYears.length - 1]
-  }, [periods])
+  const lastYears = experiences[experiences.length - 1].years
+  const lastYear = lastYears[lastYears.length - 1]
 
   return (
     <div>
@@ -69,12 +66,12 @@ export default function Resume() {
         )}
       >
         <div className="resume-item">
-          <div className="resume-item__title text-neutral-600">Hi, I am</div>
+          <div className="resume-item__title text-neutral-500">Hi, I am</div>
           <div className="resume-item__content">
             <h1 className="font-bold text-3xl">
               ASEP <span className="text-gradient-primary">FAJAR</span> NUGRAHA
             </h1>
-            <p className="text-neutral-600">Full Stack Software Engineer</p>
+            <p className="text-neutral-500">Full Stack Software Engineer</p>
           </div>
         </div>
         <div className="resume-item">
@@ -96,27 +93,34 @@ export default function Resume() {
           </div>
           <div className="resume-item__content pl-6">
             <div className="border-l-4 pl-12 space-y-6">
-              {periods.map((period, periodIndex) => (
-                <div key={`period-item-${periodIndex}`} className="relative">
+              {experiences.map((experience, experienceIndex) => (
+                <div
+                  key={`experience-item-${experienceIndex}`}
+                  className="relative"
+                >
                   <Image
                     src="/logo-shape.svg"
                     alt=""
                     width={44}
                     height={52}
                     className={`rotate-180 absolute top-0 ${
-                      periodIndex > 0
+                      experienceIndex > 0
                         ? 'w-[1.5rem] -left-[3.875rem]'
                         : 'w-[2.5rem] -left-[4.375rem]'
                     }`}
                   />
-                  <div className={`${periodIndex === 0 && 'pt-2'}`}>
+                  <div className={`${experienceIndex === 0 && 'pt-2'}`}>
                     <h3 className="text-lg font-bold">
-                      {period.years.join(' - ')}
+                      {experience.years.join(' - ')}
                     </h3>
-                    <p className="text-sm text-neutral-600">{period.summary}</p>
+                    <p className="text-sm text-neutral-500">
+                      {experience.summary}
+                    </p>
                     <ul className="list-disc pl-6 mt-2 space-y-2">
-                      {period.items.map((item, itemIndex) => (
-                        <li key={`period-item-${periodIndex}-${itemIndex}`}>
+                      {experience.items.map((item, itemIndex) => (
+                        <li
+                          key={`experience-item-${experienceIndex}-${itemIndex}`}
+                        >
                           {item}
                         </li>
                       ))}
@@ -141,25 +145,85 @@ export default function Resume() {
             </div>
           </div>
         </div>
+        <div className="resume-item !mt-16">
+          <div className="resume-item__title text-gradient-primary">stack</div>
+          <div className="resume-item__content">
+            <div className="md:flex -ml-4">
+              <div className="flex flex-col grow space-y-1 text-sm">
+                <div className="flex items-start space-x-6">
+                  <span className="w-20 shrink-0 text-neutral-500 text-right">
+                    Languages
+                  </span>
+                  <div>
+                    Programming (<strong>JS/TS</strong>, PHP, Go, Java, Kotlin,
+                    Dart, <span className="text-neutral-400">and more</span>),
+                    Other (<strong>HTML</strong>, CSS / <strong>SCSS</strong>).
+                  </div>
+                </div>
+                <div className="flex items-start space-x-6">
+                  <span className="w-20 shrink-0 text-neutral-500 text-right">
+                    Frameworks
+                  </span>
+                  <div>
+                    PHP (Laravel, CodeIgniter, Symfony), React (Next.js), Vue
+                    (Nuxt.js), Flutter, Electron, <strong>Node</strong>{' '}
+                    (Express.js, Fastify), Bun (Elysia),{' '}
+                    <span className="text-neutral-400">and more</span>.
+                  </div>
+                </div>
+                <div className="flex items-start space-x-6">
+                  <span className="w-20 shrink-0 text-neutral-500 text-right">
+                    Databases
+                  </span>
+                  <div>
+                    Relational (MySQL, <strong>PostgreSQL</strong>), NoSQL
+                    (MongoDB, Firestore, <strong>Redis</strong>).
+                  </div>
+                </div>
+                <div className="flex items-start space-x-6">
+                  <span className="w-20 shrink-0 text-neutral-500 text-right">
+                    Tools
+                  </span>
+                  <div>
+                    Docker, Kubernetes, S3, Git, CI/CD, Figma/Sketch/Adobe XD,
+                    etc.
+                  </div>
+                </div>
+                <div className="flex items-start space-x-6">
+                  <span className="w-20 shrink-0 text-neutral-500 text-right">
+                    Platforms
+                  </span>
+                  <div>DigitalOcean, GCP, Azure, AWS.</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="resume-item">
           <div className="resume-item__title text-gradient-primary">
             contact
           </div>
           <div className="resume-item__content">
-            <div className="md:flex">
-              <div className="flex flex-col grow">
-                <div className="flex">
-                  <span className="w-24 shrink-0">Address</span> Bandung, West
-                  Java, Indonesia
+            <div className="md:flex -ml-4">
+              <div className="flex flex-col grow space-y-1">
+                <div className="flex items-center space-x-6">
+                  <span className="w-20 shrink-0 text-sm text-neutral-500 text-right">
+                    Address
+                  </span>
+                  <div>Bandung, West Java, Indonesia</div>
                 </div>
-                <div className="flex">
-                  <span className="w-24 shrink-0">Mobile</span>
+                <div className="flex items-center space-x-6">
+                  <span className="w-20 shrink-0 text-sm text-neutral-500 text-right">
+                    Mobile
+                  </span>
                   <a href="https://wa.me/6285721145113" target="_blank">
                     +62 857 2114 5113
                   </a>
                 </div>
-                <div className="flex">
-                  <span className="w-24 shrink-0">Email</span>
+                <div className="flex items-center space-x-6">
+                  <span className="w-20 shrink-0 text-sm text-neutral-500 text-right">
+                    Email
+                  </span>
                   <a
                     href="mailto:delve_brain@hotmail.com"
                     target="_blank"
